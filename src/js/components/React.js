5f43b9806b1897f91ebc607ac799f8128b84add5
José Bernal <jbernal.web.dev@gmail.com>
2025-12-20 20:43:56 -0800

    Add a comparable createRoot() method.

--- src/js/components/React.js
+++ src/js/components/React.js
@@ -1,6 +1,8 @@
 
 
-const states = {};
+let states = {};
+
+
 
 
 export function setState(key, value) {
@@ -15,13 +17,32 @@ export function getState(key) {
 
 
 
-export function renderComponent(component, props) {
+export function renderComponent(component, props, prevStates) {
+    prevStates = states;
+
     let elements = component(props || {});
 
     // Get the app element from the DOM
     let app = document.getElementById("app");
 
-    // Clear existing forecast list
+
     app.innerHTML = "";
     app.appendChild(elements);
+
+
+}
+
+
+export function createRoot(rootElement) {
+
+  
+  return {
+    render: function(component) {
+      let elem = component(props || {});
+
+      rootElement.innerHTML = "";
+      rootElement.appendChild(elem);
+
+    }
+  };
 }
