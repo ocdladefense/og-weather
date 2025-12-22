ef42dde8d0896dff70ad639e498ccc1d31dd3807
José Bernal <jbernal.web.dev@gmail.com>
2025-12-22 12:10:35 -0800

    Deprecate the controller file and remove its data gathering to the main application component.

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,16 @@
-import { createRoot } from "./components/React";
+import { createRoot } from "./utils/React";
 import bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/styles.scss";
+import App from "./components/App.js";
 
 
-import Controller from "./Controller.js";
-
-let controller = new Controller();
 
 const $root = document.getElementById("app");
 const root = createRoot($root);
 
 
 
-// Initialize the Weather class on window load
 window.onload = () => {
-  controller.render();
-  // root.render(Controller);
+  root.render(App);
 };
